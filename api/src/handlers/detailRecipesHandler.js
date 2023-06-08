8147e11d47b3253e6af83ccf19be693f9d86f439
Sofiavila03 <svilapalomo@gmail.com>
2023-06-08 17:39:10 -0300

    deploy4

--- api/src/handlers/detailRecipesHandler.js
+++ api/src/handlers/detailRecipesHandler.js
@@ -1,11 +1,11 @@
-const { getRecipe} = require('../controllers/detailRecipes')
+const { getRecipe } = require('../controllers/detailRecipes')
 
-const detailRecipeHandler = async(req, res) => {
+const detailRecipeHandler = async (req, res) => {
     try {
         const detailRecipe = await getRecipe(req.params.id)
         res.status(200).json(detailRecipe)
     } catch (error) {
-        res.status(400).json({error:error.message})
+        res.status(400).json({ error: error.message })
     }
 }
 
